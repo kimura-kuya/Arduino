6d1936ca3996d73d1741a23fe5a38f86bae8d5fa
kimura-kuya <38945432+kimura-kuya@users.noreply.github.com>
2018-08-12 13:17:19 +0900

    Add files via upload
    テスト用にチューリップを流すスケッチになっています

---
+++ tonemaker.ino
@@ -0,0 +1,22 @@
+int sound[] = {262, 294, 330 , 0, 262, 294, 330 , 0, 392, 330, 294, 262, 294, 330, 294, 0, 262, 294, 330 , 0, 262, 294, 330 , 0, 392, 330, 294, 262, 294, 330, 262};
+int tonetime[] = {500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500};
+const int buzzer = 12;
+const int controll = 11;
+
+int soundsize = 30;
+
+void setup() {
+  pinMode(controll,OUTPUT);
+  
+}
+
+void loop() {
+  digitalWrite(controll,HIGH);
+  for (int i = 0; i <= soundsize; i++){
+    tone(buzzer,sound[i]);
+    delay(tonetime[i]);
+  }
+  digitalWrite(controll,LOW);
+  noTone(buzzer);
+  delay(1000);
+}
